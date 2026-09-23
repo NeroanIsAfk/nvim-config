acac30019fa178fd566030591ee185e2033096c3
Neroan <69721386+NeroanIsAfk@users.noreply.github.com>
2026-09-23 22:44:03 +0200

    Changed the way treesitter is handled to a more modern version, added some docs to it

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -18,9 +18,36 @@ if (vim.fn.has('win32') == 1) then
 	vim.env.CC = 'gcc'
 end
 
-require('nvim-treesitter').install({ 'toml', 'javascript', 'typescript', 'tsx', "html", "css", "c", "cpp", "rust", "go" })
+---The function will verify if a parser is present for a language and will launch the highlight for the language
+---@param buf integer a number that is used by Neovim
+---@param language string language name used in treesitter
+---@return boolean false if the parser isn't found in the parser runtime directory, true if the language is found and the highlight is started
+local function attach(buf, language)
+	-- check if the parser exists before starting highlighter
+	if not vim.treesitter.language.add(language) then
+		return false
+	end
+	
+	vim.treesitter.start(buf, language)
+	return true
+end
 
 vim.api.nvim_create_autocmd({"FileType"}, {
-	pattern = {"toml", "javascript", "typescript", "javascriptreact", "typescriptreact", "html", "css", "c", "cpp", "rust", "go"},
-	callback = function(ev) vim.treesitter.start(ev.buf) end
+	---The function is going to be triggered each time the user opens a file, it will check if the user has the parser for this language. If so, it is going to load the parser and starts the highlight. If not, it will try to install the parser for the language and try to start the highlight
+	callback = function(args)
+		local buf, filetype = args.buf, args.match
+		local language = vim.treesitter.language.get_lang(filetype)
+		
+		if not language then
+			return
+		end
+
+		if attach(buf, language) then
+			return
+		end
+		-- attempt to start highlighter after installing missing language
+		require('nvim-treesitter').install(language):await(function()
+			attach(buf, language)
+		end)
+	end,
 })
