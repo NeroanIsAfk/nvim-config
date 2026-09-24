35054bf84ea8b7fc8c0f8d971fb7e52413b58ca3
Neroan <charlier.killian@outlook.fr>
2026-09-24 11:47:02 +0200

    added colorscheme and added bufferline but not working

--- init.lua
+++ init.lua
@@ -1,8 +1,12 @@
 vim.opt.number = true
 vim.opt.clipboard = "unnamedplus"
 vim.g.mapleader = " "
+vim.opt.termguicolors = true
 
 require('plugins.lsp')
 require('plugins.treesitter')
 require('plugins.telescope')
 require('plugins.mini_pairs')
+require('plugins.colorscheme')
+-- not working
+require('plugins.bufferline')
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,5 @@
+vim.pack.add({ 'https://github.com/EdenEast/nightfox.nvim' })
+
+-- dayfox, dawnfox, nightfox, duskfox, nordfox, terafox, carbonfox
+
+vim.cmd("colorscheme dawnfox")
